da11070ebfd4bffaa258d3b3779094d17e40c73f
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 20:30:58 +0530

    Create Sum_Recursion.cpp

---
+++ Sum_Recursion.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int add(int n){
+    if(n<=1){ //Terminating statement
+        return 1;
+    }else{
+        return (n+add(n-1)); //Recursion
+    }
+}
+
+int main(){
+    int X,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    X= add(n); //Function calling
+    cout<<X;
+    return 0;
+}
+
+/*OUTPUT
+Enter a number: 5
+15
+*/
